513cd31ed51634b4a1aa450f15c1a6d119994115
Tyler Barlow <barlowtyler96@gmail.com>
2023-05-11 17:47:58 -0400

    Refactored. Added using statements to Dispose commands

--- HabitTracker.barlowtyler96/HabitTracker.barlowtyler96/Program.cs
+++ HabitTracker.barlowtyler96/HabitTracker.barlowtyler96/Program.cs
@@ -1,34 +1,15 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using HabitTracker.barlowtyler96.Controller;
+using HabitTracker.barlowtyler96.View;
+using System.Configuration;
 
-namespace HabitTracker
+namespace HabitTracker;
+
+internal class Program
 {
-    internal class Program
+    private static readonly string connectionString = ConfigurationManager.AppSettings.Get("connString");
+    static void Main(string[] args)
     {
-        public static string ConnectionString { get; private set; } = @"Data Source=habit-Tracker.db";
-
-        static void Main(string[] args)
-        {
-            //Using statement calls Dispose() after the using block is left.
-            using (var connection = new SqliteConnection(ConnectionString))
-            {
-                connection.Open();
-                var tableCmd = connection.CreateCommand(); //Creates a command to send to DB
-
-                tableCmd.CommandText = //Defines the command string to create a table
-                    @"CREATE TABLE IF NOT EXISTS habits
-                        (
-                        Id INTEGER PRIMARY KEY AUTOINCREMENT,
-                        Date TEXT,
-                        Activity TEXT,
-                        Unit TEXT,
-                        Amount INTEGER
-                        )";
-
-                tableCmd.ExecuteNonQuery();//Executes the command without returning a value. Only telling it to create a table.
-
-                connection.Close(); //Closes the connection with the DB
-            }
-            MainMenu.GetUserInput();
-        }
+        DbController.CreateTable();
+        MainMenu.GetUserInput();
     }
 }
